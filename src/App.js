d84dcf55a26cc92fea15171be8444923b1b354e8
Missae Sawaguchi <missae.sawaguchi@gmail.com>
2020-05-24 20:00:16 -0300

    Some visual changes

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React, { useEffect, useState } from 'react';
+import React, { useEffect, useState } from "react";
 import {
   SafeAreaView,
   View,
@@ -7,15 +7,18 @@ import {
   StatusBar,
   StyleSheet,
   TouchableOpacity,
-} from 'react-native';
-import api from './services/api';
+} from "react-native";
+import Icon from "react-native-vector-icons/AntDesign";
+import api from "./services/api";
 
 export default function App() {
   const [repositories, setRepositories] = useState([]);
 
   useEffect(() => {
-    api.get('repositories').then((response) => {
+    api.get("repositories").then((response) => {
       setRepositories(response.data);
+      console.log(response.data);
+      console.log(repositories);
     });
   }, []);
 
@@ -40,7 +43,6 @@ export default function App() {
           data={repositories}
           keyExtractor={(repository) => repository.id}
           renderItem={({ item: repository }) => (
-
             <View style={styles.repositoryContainer}>
               <Text style={styles.repository}>{repository.title}</Text>
 
@@ -53,29 +55,33 @@ export default function App() {
               </View>
 
               <View style={styles.likesContainer}>
+                <Icon name="like1" size={18} color="#707079" />
                 <Text
                   style={styles.likeText}
                   testID={`repository-likes-${repository.id}`}
                 >
-                  {repository.likes}
-                  {' '}
-                  curtida
-                  {repository.likes > 1 ? 's' : ''}
+                  {repository.likes} curtida
+                  {repository.likes > 1 ? "s" : ""}
                 </Text>
               </View>
 
               <TouchableOpacity
-                style={styles.button}
                 onPress={() => handleLikeRepository(repository.id)}
                 testID={`like-button-${repository.id}`}
               >
-                <Text style={styles.buttonText}>Curtir</Text>
+                <View style={styles.likebutton}>
+                  <Icon
+                    name="like1"
+                    size={20}
+                    color="#fff"
+                    style={styles.iconbutton}
+                  />
+                  <Text style={styles.buttonText}> CURTIR</Text>
+                </View>
               </TouchableOpacity>
             </View>
-
           )}
         />
-
       </SafeAreaView>
     </>
   );
@@ -84,50 +90,65 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#7159c1',
+    backgroundColor: "#463774",
   },
   repositoryContainer: {
     marginBottom: 15,
     marginHorizontal: 15,
-    backgroundColor: '#fff',
+    backgroundColor: "#fff",
     padding: 20,
+    borderRadius: 10,
   },
   repository: {
-    fontSize: 32,
-    fontWeight: 'bold',
+    fontSize: 26,
+    fontWeight: "bold",
+    color: "#565662",
   },
   techsContainer: {
-    flexDirection: 'row',
+    flexDirection: "row",
     marginTop: 10,
   },
   tech: {
     fontSize: 12,
-    fontWeight: 'bold',
+    fontWeight: "bold",
     marginRight: 10,
-    backgroundColor: '#04d361',
+    backgroundColor: "#04d361",
     paddingHorizontal: 10,
     paddingVertical: 5,
-    color: '#fff',
+    color: "#fff",
+    borderRadius: 10,
   },
   likesContainer: {
     marginTop: 15,
-    flexDirection: 'row',
-    justifyContent: 'space-between',
+    flexDirection: "row",
   },
   likeText: {
     fontSize: 14,
-    fontWeight: 'bold',
+    fontWeight: "bold",
     marginRight: 10,
+    marginLeft: 6,
+    marginTop: 2,
+    color: "#565662",
   },
-  button: {
-    marginTop: 10,
+  likebutton: {
+    marginTop: 20,
+    flexDirection: "row",
+    borderRadius: 10,
+    backgroundColor: "#7159c1",
+    justifyContent: "center",
   },
+
   buttonText: {
     fontSize: 14,
-    fontWeight: 'bold',
+    fontWeight: "bold",
     marginRight: 10,
-    color: '#fff',
-    backgroundColor: '#7159c1',
+    color: "#fff",
     padding: 15,
+    borderRadius: 10,
+    justifyContent: "space-around",
+  },
+
+  iconbutton: {
+    marginTop: 12,
   },
 });
